a98e9eb0267732b2f41e6209abdaaaac9e2be065
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2020-11-21 10:02:49 +1300

    Added Int64 buffer support with tests.  Tests extracted to separate unit

--- tests/test.dpr
+++ tests/test.dpr
@@ -7,47 +7,8 @@
 
 uses
   Deltics.Smoketest,
-  Deltics.ReverseBytes in '..\src\Deltics.ReverseBytes.pas';
-
-type
-    TTests = class(TTest)
-      procedure ReverseBytesOverAWordReturnsAWordWithBytesSwapped;
-      procedure ReverseBytesOverALongWordReturnsALongWordWithBytesSwapped;
-      procedure ReverseBytesOverAnInt64ReturnsAnInt64WithBytesSwapped;
-    end;
-
-
-
-  procedure TTests.ReverseBytesOverALongWordReturnsALongWordWithBytesSwapped;
-  var
-    v: Integer;
-  begin
-    v := ReverseBytes($01020304);
-
-    Test('result').Assert(v).Equals($04030201);
-  end;
-
-
-  procedure TTests.ReverseBytesOverAnInt64ReturnsAnInt64WithBytesSwapped;
-  var
-    v: Int64;
-  begin
-    v := ReverseBytes($0102030405060708);
-
-    Test('result').Assert(v).Equals($0807060504030201);
-  end;
-
-
-  procedure TTests.ReverseBytesOverAWordReturnsAWordWithBytesSwapped;
-  var
-    v: Word;
-  begin
-    v := ReverseBytes($0102);
-
-    Test('result').Assert(v).Equals($0201);
-  end;
-
-
+  Deltics.ReverseBytes in '..\src\Deltics.ReverseBytes.pas',
+  Tests in 'Tests.pas';
 
 
 begin
